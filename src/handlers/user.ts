537bd26cbdc5d7a2a9ebcc41bf812e322c13e21d
romuloban <romulo88@hotmail.com>
2024-06-07 20:02:37 +0200

    add user handlers

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,5 +1,5 @@
 import prisma from "../db"
-import { createJWT, hashPassword } from "../modules/auth"
+import { comparePassword, createJWT, hashPassword } from "../modules/auth"
 
 export const newUser = async (req, res) => {
   const {username, password} = req.body
@@ -9,6 +9,31 @@ export const newUser = async (req, res) => {
       password: await hashPassword(password)
     }
   })
+  
+  const token = createJWT(user)
+  res.json({token})
+}
+
+export const signin = async (req, res) => {\
+  const {username, password} = req.body
+  const user = await prisma.user.findUnique({
+    where: {
+      username
+    }
+  })
+  
+  if (!user) {
+    return res.status(401).json({
+      message: 'not authorized'
+    })
+  }
+  
+  const valid = await comparePassword(password, user.password)
+  if (!valid) {
+    return res.status(401).json({
+      message: 'not authorized'
+    })
+  }
   
   const token = createJWT(user)
   res.json({token})
