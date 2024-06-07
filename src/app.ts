0ab2218fab5533fec745fbc8544f6b5255f19bf1
romuloban <romulo88@hotmail.com>
2024-06-07 18:19:37 +0200

    add some middleware

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import morgan from 'morgan'
 const app = express()
 // Middleware
 app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.use('/api', router)
 
